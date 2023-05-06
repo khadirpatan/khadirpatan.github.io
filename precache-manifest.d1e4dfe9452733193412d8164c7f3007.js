1b4ac64d66492b682e284e98bf64a761aafdb495
khadirpatan <khadirpatan1@gmail.com>
2023-05-07 03:21:54 +0530

    Updates

---
+++ precache-manifest.d1e4dfe9452733193412d8164c7f3007.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "491fb8a04a150d58ca0c",
+    "url": "/static/css/main.5ebac3f0.chunk.css"
+  },
+  {
+    "revision": "491fb8a04a150d58ca0c",
+    "url": "/static/js/main.491fb8a0.chunk.js"
+  },
+  {
+    "revision": "f298a5e612009a5932e6",
+    "url": "/static/css/1.0f9eb1b5.chunk.css"
+  },
+  {
+    "revision": "f298a5e612009a5932e6",
+    "url": "/static/js/1.f298a5e6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8e1f02690f5908ba5545d1304d1521b3",
+    "url": "/static/media/bg1.8e1f0269.jpg"
+  },
+  {
+    "revision": "0a47b25a2f5842d4cf9691e090f6537e",
+    "url": "/static/media/myimg.0a47b25a.jpg"
+  },
+  {
+    "revision": "07270c13412737933c6f3468d97618ad",
+    "url": "/static/media/css.07270c13.png"
+  },
+  {
+    "revision": "9dc36c7b5ff5db2c525871b916d64f24",
+    "url": "/static/media/javascript.9dc36c7b.png"
+  },
+  {
+    "revision": "3851cc047b9284b97c521cc1a7d8a512",
+    "url": "/static/media/mongodb.3851cc04.png"
+  },
+  {
+    "revision": "067a4c5e08820e4967db5d0032562e1e",
+    "url": "/static/media/shipnext.067a4c5e.png"
+  },
+  {
+    "revision": "ebbd1a0a7836873dc69b3e50c44d8917",
+    "url": "/static/media/shipnextOfc.ebbd1a0a.png"
+  },
+  {
+    "revision": "95597ba4cfd87e68d790f0ce8e94b77d",
+    "url": "/static/media/shoppingCart.95597ba4.png"
+  },
+  {
+    "revision": "cbf6c04210a869131fee9ea00401b677",
+    "url": "/static/media/sneed.cbf6c042.png"
+  },
+  {
+    "revision": "44cc665d008dd3ce30ebbf0aa628dc9a",
+    "url": "/static/media/moviesearch.44cc665d.PNG"
+  },
+  {
+    "revision": "3a56ab6878714f14bf0dbfe8d0ac94e2",
+    "url": "/static/media/portfolio.3a56ab68.PNG"
+  },
+  {
+    "revision": "a9ea26c4a8db2beeab9565bc9533dd00",
+    "url": "/static/media/whatsaap.a9ea26c4.PNG"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "42479cc14ae93d85ca1333bbdf12a0a1",
+    "url": "/index.html"
+  }
+];
